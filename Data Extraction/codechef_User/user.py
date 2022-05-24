a8afd8a2ab03728a3e6bd7c9228d6a32daf2026f
Pprince_be19@thapar.edu <pprince_be19@thapar.edu>
2022-05-24 09:44:09 +0530

    data extraction copy_data,except, log file,user.py

---
+++ Data Extraction/codechef_User/user.py
@@ -0,0 +1,40 @@
+import MySQLdb
+
+class UserSubmission:
+	"""docstring for ClassName"""
+	def __init__(self, problemCode, noOfSubmissions, time):
+		self.problemCode = problemCode
+		self.noOfSubmissions = noOfSubmissions
+		self.time = time
+
+	def __str__(self):
+		return "Problem code: " + self.problemCode + " No of submissions: " + str(self.noOfSubmissions) + " Time: "+str(self.time)
+
+class User:
+	baseUrl = "https://www.codechef.com/users/"
+	#ratingType = ['long', 'short', 'ltime']
+
+	def __init__(self, uname, country, city, isStudent, successSubmissions, prefLang, ratings, ranks):
+		self.uname = uname
+		self.url = self.baseUrl + uname
+
+		self.country = country
+		
+		self.city = city
+		self.isStudent = isStudent
+		
+		self.successSubmissions = successSubmissions
+		self.prefLang = prefLang
+
+		self.ratings = ratings
+		self.ranks = ranks
+
+
+	def __str__(self):
+		s = 'Username: '+self.uname+' Country: '+self.country+' City: '+self.city+' isStudent'+str(self.isStudent)\
+		+ ' Prefered Language: ' + self.prefLang
+		s = s.encode('utf-8', 'ignore')
+		#s = unicode(s, errors='ignore')
+		return s
+
+			
