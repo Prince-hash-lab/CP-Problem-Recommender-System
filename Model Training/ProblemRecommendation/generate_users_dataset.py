6dcff95438e26a2b11fd9ba4942dcbf40dd0a7e6
Pprince_be19@thapar.edu <pprince_be19@thapar.edu>
2022-07-16 22:09:02 +0530

    generate user data base and recommend.py added

---
+++ Model Training/ProblemRecommendation/generate_users_dataset.py
@@ -0,0 +1,137 @@
+from get_probs import get_all_probs_without_category_NA
+
+import pickle
+
+import sys, csv, os, random
+
+sys.path.append('Utilities/')
+from constants import categories, codechefDifficultyLevels, PlatformType, defaultTestSize, categoryWiseWeights
+from get_users import get_codechef_users
+from user_class import Codechef_User
+
+userNameToObjects = None
+userNameToObjectsAll = None
+
+
+def get_userNameToObjects(uniqueFileConvention, platform):
+    if platform == PlatformType.Codechef:
+        users = None
+        if not os.path.isfile(uniqueFileConvention + '_orm.pickle'):
+            print(uniqueFileConvention + '_orm.pickle ' + 'not found')
+            userNameToObjects, probCodeToObjects = get_codechef_users()
+            with open(uniqueFileConvention + '_orm.pickle', 'wb') as f:
+                print('Dumping ' + uniqueFileConvention + '_orm.pickle')
+                pickle.dump(userNameToObjects, f)
+        else:
+            with open(uniqueFileConvention + '_orm.pickle', 'rb') as f:
+                print('Loading from ' + uniqueFileConvention + '_orm.pickle')
+                userNameToObjects = pickle.load(f)
+
+    return userNameToObjects
+
+def get_probCodeToObjects():
+    with open('users/' + 'codechef_probs.pickle', 'rb') as f:
+        probCodeToObjects = pickle.load(f)
+
+    with open('users/' + 'codechef_probs_all.pickle', 'rb') as f:
+        probCodeToObjectsAll = pickle.load(f)
+
+    return probCodeToObjects, probCodeToObjectsAll
+
+
+def generateLazyLoad(uniqueFileConvention, platform=PlatformType.Codechef, username='none'):
+    global userNameToObjects
+
+    # if userNameToObjects is None:
+    #     if platform == PlatformType.Codechef:
+    #         users = None
+    userObject = None
+
+    if not os.path.isfile('users/'+ uniqueFileConvention + '_orm.pickle'):
+        print(uniqueFileConvention + '_orm.pickle ' + 'not found')
+        userNameToObjects, probCodeToObjects = get_codechef_users(probs_all_or_categorywise=1)
+        print('Dumping ' + uniqueFileConvention + '_orm.pickle')
+        with open('users/' + uniqueFileConvention + '_orm.pickle', 'wb') as f:
+            pickle.dump(userNameToObjects, f)
+
+        with open('users/' + 'codechef_probs.pickle', 'wb') as f:
+            pickle.dump(probCodeToObjects, f)
+
+        for user in userNameToObjects:
+            with open('users/' + uniqueFileConvention + '_' + user + '.pickle', 'wb') as f:
+                pickle.dump(userNameToObjects[user], f)
+
+    if userNameToObjects is not None:
+        return userNameToObjects
+
+    if username == 'none':
+        with open('users/' + uniqueFileConvention + '_orm.pickle', 'rb') as f:
+            print('Loading from ' + uniqueFileConvention + '_orm.pickle')
+            userNameToObjects = pickle.load(f)
+        return userNameToObjects
+    else:
+        with open('users/' + uniqueFileConvention + '_' + username + '.pickle', 'rb') as f:
+            userObject = pickle.load(f)
+            # write_dataset(uniqueFileConvention, userNameToObjects, platform)
+        return userObject
+
+
+def generateLazyLoadAll(uniqueFileConvention, platform=PlatformType.Codechef, username='none'):
+    global userNameToObjectsAll
+
+    # if userNameToObjectsAll is None:
+    #     if platform == PlatformType.Codechef:
+    #         users = None
+
+    userObjectsAll = None
+
+    if not os.path.isfile('users/' + uniqueFileConvention + '_orm.pickle'):
+        print(uniqueFileConvention + '_orm.pickle ' + 'not found')
+        userNameToObjectsAll, probCodeToObjects = get_codechef_users(probs_all_or_categorywise=2)
+        print('Dumping ' + uniqueFileConvention + '_orm.pickle')
+
+        with open('users/' + uniqueFileConvention + '_orm.pickle', 'wb') as f:
+            pickle.dump(userNameToObjectsAll, f)
+
+        with open('users/' + 'codechef_probs_all.pickle', 'wb') as f:
+            pickle.dump(probCodeToObjects, f)
+
+        for user in userNameToObjectsAll:
+            with open('users/' + uniqueFileConvention + '_' + user + '.pickle', 'wb') as f:
+                pickle.dump(userNameToObjects[user], f)
+
+    if userNameToObjectsAll is not None:
+        return userNameToObjectsAll
+
+    if username == 'none':
+        with open('users/' + uniqueFileConvention + '_orm.pickle', 'rb') as f:
+            print('Loading from ' + uniqueFileConvention + '_orm.pickle')
+            userNameToObjectsAll = pickle.load(f)
+        return userNameToObjectsAll
+    else:
+        with open('users/' + uniqueFileConvention + '_' + username + '.pickle', 'rb') as f:
+            userObjectsAll = pickle.load(f)
+            # write_dataset(uniqueFileConvention, userNameToObjects, platform)
+        return userObjectsAll
+
+
+def write_dataset(uniqueFileConvention, userNameToObjects, platform=PlatformType.Codechef):
+    print('Writing dataset')
+    with open(uniqueFileConvention + '_dataset.csv', 'w') as f:
+        f.write('uname')
+        for category in categories:
+            for level in codechefDifficultyLevels:
+                f.write(',' + category + '_' + level)
+        f.write('\n')
+        for username in userNameToObjects:
+            user = userNameToObjects[username]
+            f.write(user.uname)
+            for category in user.categoryDifficultyMap:
+                i = 0
+                for level in user.categoryDifficultyMap[category]:
+                    value = len(user.categoryDifficultyMap[category][level]) * categoryWiseWeights[category]\
+                                                                * categorywise_difficulty_weights[category][i]
+                    f.write(',' + str(value))
+                    i += 1
+            f.write('\n')
+        print('User Dataset written: ' + uniqueFileConvention)
---
+++ Model Training/ProblemRecommendation/recommend.py
@@ -0,0 +1,249 @@
+from gensim import models
+import pickle
+import operator
+import sys
+import copy
+
+from sqlalchemy.sql.functions import user
+
+from sorting import sort_by_date_difficulty, sort_by_date
+from generate_users_dataset import generateLazyLoad, generateLazyLoadAll, get_probCodeToObjects
+from get_probs import get_difficulty, get_category, get_all_probs_without_category_NA
+from user_level_limits import get_categorywise_difficulty_limits, get_difficulty_limits_without_category
+
+from recommend_popular import get_popular
+
+sys.path.append('Utilities/')
+
+from constants import PlatformType, categories, stddifficultyLevels, codechefDifficultyLevels
+tp = tn = fp = fn = 0
+
+def build_recommendation_list_for_users(usersClusterMap, probs):
+    probDict = {}
+    for prob in probs:
+        probDict[prob.prob_code] = prob
+
+    for label in usersClusterMap:
+        userList = usersClusterMap[label]
+        for user in userList:
+            user.calculate_user_level()
+            print(user.uname + ' ' + str(len(user.failed_probs)) + ' ' + str(len(user.solved_probs)))
+            if len(user.failed_probs) == 0 and len(user.solved_probs) > 0:
+                print(user.uname + ' Level: ' + str(user.user_level))
+
+        userList.sort(key=lambda x: x.user_level, reverse=True)
+        # print(userList)
+
+    for label in usersClusterMap:
+        userList = usersClusterMap[label]
+
+        probsSolvedUntilCurrentLevelUser = []
+        generalUsersStartIndex = int(len(userList) * 0.1) + 1
+        for i in range(len(userList)):
+            tempDict = {}
+            if i >= generalUsersStartIndex:
+                for probCode in probsSolvedUntilCurrentLevelUser:
+                    if probCode not in userList[i].solved_probs:
+                        tempDict[probCode] = probDict[probCode].get_problem_level()
+
+                        tempDict = dict(sorted(tempDict.items(), key=operator.itemgetter(1), reverse=True))
+                        # print(str(tempDict))
+            for probCode in tempDict:
+                userList[i].recommendation_list.append(probCode)
+            # print(str(userList[i].recommendation_list))
+
+            for probCode in userList[i].solved_probs:
+                probsSolvedUntilCurrentLevelUser.append(probCode)
+
+        usersClusterMap[label] = userList
+        return usersClusterMap
+
+
+def get_word2vec_recommendation(uniqueFileConvention, submissionsDict, prev_sub, diff, no_recomm):
+    model = models.Word2Vec.load(uniqueFileConvention)
+
+    sorted_submissions = sort_by_date_difficulty(submissionsDict, diff)
+    recommendation = None
+
+    if len(sorted_submissions) >= prev_sub:
+        submission_sentence = []
+        [submission_sentence.append(row[0]) for row in sorted_submissions[-prev_sub:]]
+
+        # Word2Vec function call to give recommendations
+        recommendation = model.most_similar(positive=submission_sentence, topn=no_recomm)
+        # recommendation = model.most_similar_cosmul(positive=submission_sentence, topn=no_recomm)
+
+
+    else:
+        submission_sentence = []
+        sorted_submissions = sort_by_date_difficulty(submissionsDict)
+        [submission_sentence.append(row[0]) for row in sorted_submissions[-prev_sub:]]
+        recommendation = model.most_similar(positive=submission_sentence, topn=no_recomm)
+
+    return recommendation
+
+
+def get_user_level_without_category(uniqueFileConvention, level_wise_submissions):
+    optimum_level = 'easy'
+
+    difficulty_limits = get_difficulty_limits_without_category(uniqueFileConvention, PlatformType.Codechef, 730)
+
+    if level_wise_submissions['medium'] >= difficulty_limits['medium']:
+        optimum_level = 'hard'
+
+    elif level_wise_submissions['easy'] >= difficulty_limits['easy']:
+        optimum_level = 'medium'
+
+    return optimum_level
+
+
+def get_user_level_by_category(uniqueFileConvention, categoryDifficultyMap):
+    optimum_category_level = {'easy': [], 'medium': [], 'hard': []}
+
+    categorywise_difficulty_limits = get_categorywise_difficulty_limits(uniqueFileConvention, PlatformType.Codechef,
+                                                                        730)
+    for category in categoryDifficultyMap:
+
+        if categoryDifficultyMap[category]['medium'] >= categorywise_difficulty_limits[category]['medium']:
+            optimum_category_level['hard'].append(category)
+        elif categoryDifficultyMap[category]['easy'] >= categorywise_difficulty_limits[category]['easy']:
+            optimum_category_level['medium'].append(category)
+        else:
+            optimum_category_level['easy'].append(category)
+
+    # print "Optimum level for each category"
+    # print optimum_category_level
+
+    return optimum_category_level
+
+def print_recommendation(recommendation):
+    for row in recommendation:
+        val = ''
+        val += str(row) + " "
+        for cat in get_category(row[0]):
+            val +=  cat + " "
+        val += get_difficulty(row[0])
+        print val
+
+def recommender(uniqueFileConvention, userObjects, username):
+
+    global tp, tn, fp, fn
+
+    submissionsDict = userObjects.problemMappings
+    optimum_category_level = get_user_level_by_category(uniqueFileConvention, userObjects.categoryDifficultyMap)
+
+    # evalsubmissions = []
+    # for x in sort_by_date(submissionsDict)[-10:]:
+    #     evalsubmissions.append(x[0])
+    #
+    # for prob_code in evalsubmissions:
+    #     del submissionsDict[prob_code]
+
+    # print "Category wise difficulty limits"
+    # print categorywise_difficulty_limits
+    # print "User submissions sorted by category and difficulty"
+    # print categoryDifficultyMap
+
+    final_recommendation = []
+
+    if len(submissionsDict) < 5:
+
+        uniqueFileConvention1 = 'users_codechef_all_probs'
+        userObjectsAll = generateLazyLoadAll(uniqueFileConvention1, platform=PlatformType.Codechef, username=username)
+        submissionsDict = userObjectsAll.problemMappings
+        if len(submissionsDict) < 5:
+            print "Less than 5 submissions for this user"
+            for category in categories:
+                final_recommendation.append(get_popular('easy', category, final_recommendation, {}))
+
+        else:
+            level = get_user_level_without_category(uniqueFileConvention, userObjectsAll[user].level_wise_submissions)
+            recommendation = get_word2vec_recommendation(uniqueFileConvention1, submissionsDict, 5, level, 5)
+
+            for problem in recommendation:
+                final_recommendation.append(problem[0])
+
+    else:
+
+        all_recommendation = []
+        iterations = 0
+        obtained_categories = 0
+        recomm_no_probs_per_category = 3
+        prev_sub = 0
+        no_recomm = 100
+        score_threshold = 0.75
+        optimum_category_level_copy = copy.deepcopy(optimum_category_level)
+        categorywise_recomm = {}
+        for category in categories:
+            categorywise_recomm[category] = []
+        while obtained_categories != 6 and iterations != 10:
+            iterations += 1
+            prev_sub += 5
+            for level in optimum_category_level:
+                if len(optimum_category_level[level]) != 0:
+                    recommendation = get_word2vec_recommendation(uniqueFileConvention, submissionsDict, prev_sub, level, no_recomm)
+                    if recommendation is not None:
+                        for problem in recommendation:
+                            if problem[1] >= score_threshold:
+                                if get_difficulty(problem[0]) == level:
+                                    rec_prob_cat = get_category(problem[0])
+                                    for cat in rec_prob_cat:
+                                        if cat in optimum_category_level[level]:
+                                            if problem[0] not in all_recommendation:
+                                                categorywise_recomm[cat].append(problem)
+                                                all_recommendation.append(problem[0])
+                                                if len(categorywise_recomm[cat]) == recomm_no_probs_per_category:
+                                                    optimum_category_level[level].remove(cat)
+                                                    obtained_categories += 1
+
+                            else:
+                                break
+                    else:
+                        # Low submission count for a particular level
+                        obtained_categories += len(optimum_category_level[level])
+                        optimum_category_level[level][:] = []
+
+                        break
+
+        for level in stddifficultyLevels:
+            for category in optimum_category_level_copy[level]:
+                if categorywise_recomm[category]:
+                    final_recommendation.append(categorywise_recomm[category][0][0])
+                else:
+                    prob_code = get_popular(level, category, all_recommendation, userObjects.solved_probs)
+                    final_recommendation.append(prob_code)
+
+
+    print final_recommendation
+    # print evalsubmissions
+
+    recomm_prob_obj = []
+
+    probObjects, probObjectsAll = get_probCodeToObjects()
+
+    for prob_code in final_recommendation:
+        try:
+            recomm_prob_obj.append(probObjects[prob_code])
+        except:
+            recomm_prob_obj.append(probObjectsAll[prob_code])
+
+    print("=====================================================================================")
+
+    return recomm_prob_obj
+
+
+def get_recommendations(username):
+
+    uniqueFileConvention = 'users_codechef'
+    platform = PlatformType.Codechef
+
+    userObjects = generateLazyLoad(uniqueFileConvention, platform, username)
+
+    recommended_probs = recommender(uniqueFileConvention, userObjects, username)
+
+    return recommended_probs, userObjects.categoryDifficultyMap, sort_by_date(userObjects.problemMappings)
+
+
+if __name__ == '__main__':
+
+    get_recommendations("i_am_what_i_am")
